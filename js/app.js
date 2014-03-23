98d148ce4fbc8f34ba152712740ca5df4eea6d0b
Kevin Lin <whatakevin@gmail.com>
2014-03-23 18:58:45 -0500

    Add animations

---
+++ js/app.js
@@ -0,0 +1,24 @@
+var kevinApp = angular.module("kevinApp", ['ngAnimate']);
+
+kevinApp.controller("kevinController", function($scope){
+
+});
+
+// connect animations
+$('#github').addClass("animated fadeIn");
+$('#linkedin').addClass("animated fadeIn");
+$('#facebook').addClass("animated fadeIn");
+
+// featurette animations
+$('#utarl-featurette').waypoint(function(){
+  $('#utarl-featurette').addClass("animated fadeInRight");
+}, { offset: '85%' });
+$('#surewalk-featurette').waypoint(function(){
+  $('#surewalk-featurette').addClass("animated fadeInLeft");
+}, { offset: '85%' });
+$('#christmas-featurette').waypoint(function(){
+  $('#christmas-featurette').addClass("animated fadeInRight");
+}, { offset: '85%' });
+$('#sanger-featurette').waypoint(function(){
+  $('#sanger-featurette').addClass("animated fadeInLeft");
+}, { offset: '85%' });
